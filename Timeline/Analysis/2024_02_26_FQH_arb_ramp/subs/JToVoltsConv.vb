fea8076042f6d7970d214d8adca37a4d62c37511
PerrinSeg <87147259+PerrinSeg@users.noreply.github.com>
2024-02-27 20:27:18 -0500

    fixed loop index issue with JToVoltsConv

--- Timeline/Analysis/2024_02_26_FQH_arb_ramp/subs/JToVoltsConv.vb
+++ Timeline/Analysis/2024_02_26_FQH_arb_ramp/subs/JToVoltsConv.vb
@@ -5,9 +5,9 @@ Public Function JToVolts(ByVal desired_j As Double, _
                          ByVal calib_volt As Double, _
                          ByVal offset_volt As Double) As Double
 
-    Dim nterms As Integer = (ncoeffs - 1)/2
+    Dim nterms As Integer = (ncoeffs)/2
     Dim desired_depth As Double = coeffs(0)
-    For index As Integer = 1 To nterms + 1
+    For index As Integer = 1 To nterms
         desired_depth = desired_depth + coeffs(2 * index - 1) * Exp(-desired_j / coeffs(2 * index))
     Next
     Dim volts As Double = offset_volt + calib_volt + 0.5*Log10(desired_depth/calib_depth)
