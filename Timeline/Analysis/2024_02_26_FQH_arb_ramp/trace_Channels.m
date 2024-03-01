0a4c16a3dae04a2768587363892e6d2ee3b544f8
PerrinSeg <87147259+PerrinSeg@users.noreply.github.com>
2024-03-01 13:04:37 -0500

    fixed Exp Constants

--- Timeline/Analysis/2024_02_26_FQH_arb_ramp/trace_Channels.m
+++ Timeline/Analysis/2024_02_26_FQH_arb_ramp/trace_Channels.m
@@ -23,6 +23,8 @@ findIndex = @(list, element) find(strcmp(cellfun(firstCell, list, 'UniformOutput
 % Where all the functions are defined
 % addpath('Z:\Timeline\Subs\') 
 addpath('C:\Users\Rb Lab\Documents\GitHub\fqh_arbitrary_ramps\Timeline\Subs\') 
+% addpath('C:\Users\Perrin\Documents\GitHub\fqh_arbitrary_ramps\Timeline\Subs\') 
+
 [instruction_list, arguments_list, variable_list, arr_variable_list, sub_variable_containers, ExpConstants, LogExpParam] ...
     = read_Sequence(path_files, name_sequence, batch_line);
 N_inst = numel(instruction_list);
