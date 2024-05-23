ebb7ad73b9a7553e9f6c4c4128bfc10cff72ab48
PerrinSeg <87147259+PerrinSeg@users.noreply.github.com>
2024-05-23 15:36:53 -0400

    final version of convert_ramp_to_single_files
    fully functional conversion program, accounts for required normalization to run using NI function AddInterpolatedRampUsingFile (i.e. start is 1, stop is 0, reverse ramps that stop higher than they start).

--- Timeline/Analysis/2024_05_22_Tizian_adiabatic_ramp_conversion_test/FQH_Tizian_adiabatic_analog.vb
+++ Timeline/Analysis/2024_05_22_Tizian_adiabatic_ramp_conversion_test/FQH_Tizian_adiabatic_analog.vb
@@ -15,10 +15,10 @@ ramp_dur = 500
 '----------------------------------------------------- Arbitrary ramp constants --------------------------------------------------------------
 '---------------------------------------------------------------------------------------------------------------------------------------------
 
-Dim quicTiltRampPath As String = "C:\\Users\\Perrin\\Documents\\GitHub\\fqh_arbitrary_ramps\\Timeline\\Analysis\\2024_05_22_Tizian_adiabatic_ramp_conversion_test\\convert_ramp_files\\DVy_ramp.txt"
-Dim quadTiltRampPath As String = "C:\\Users\\Perrin\\Documents\\GitHub\\fqh_arbitrary_ramps\\Timeline\\Analysis\\2024_05_22_Tizian_adiabatic_ramp_conversion_test\\convert_ramp_files\\DVx_ramp.txt"
-Dim quadTunnelRampPath As String = "C:\\Users\\Perrin\\Documents\\GitHub\\fqh_arbitrary_ramps\\Timeline\\Analysis\\2024_05_22_Tizian_adiabatic_ramp_conversion_test\\convert_ramp_files\\KVx_ramp.txt"
-Dim quicTunnelRampPath As String = "C:\\Users\\Perrin\\Documents\\GitHub\\fqh_arbitrary_ramps\\Timeline\\Analysis\\2024_05_22_Tizian_adiabatic_ramp_conversion_test\\convert_ramp_files\\JVy_ramp.txt"
+Dim quicTiltRampPath As String = "C:\\Users\\Rb Lab\\Documents\\GitHub\\fqh_arbitrary_ramps\\Timeline\\Analysis\\2024_05_22_Tizian_adiabatic_ramp_conversion_test\\convert_ramp_files\\DVy_ramp.txt"
+Dim quadTiltRampPath As String = "C:\\Users\\Rb Lab\\Documents\\GitHub\\fqh_arbitrary_ramps\\Timeline\\Analysis\\2024_05_22_Tizian_adiabatic_ramp_conversion_test\\convert_ramp_files\\DVx_ramp.txt"
+Dim quadTunnelRampPath As String = "C:\\Users\\Rb Lab\\Documents\\GitHub\\fqh_arbitrary_ramps\\Timeline\\Analysis\\2024_05_22_Tizian_adiabatic_ramp_conversion_test\\convert_ramp_files\\KVx_ramp.txt"
+Dim quicTunnelRampPath As String = "C:\\Users\\Rb Lab\\Documents\\GitHub\\fqh_arbitrary_ramps\\Timeline\\Analysis\\2024_05_22_Tizian_adiabatic_ramp_conversion_test\\convert_ramp_files\\JVy_ramp.txt"
 
 Dim lattice1_max As Double = 45
 Dim lattice2_max As Double = 45
@@ -54,7 +54,7 @@ Dim ramp_end_time As Double = ramp_start_time + ramp_dur
 '----------------------------------------------------- Pinning -------------------------------------------------------------------------------
 
 '(3) pinning
-Dim pinning_dur As Double = 1000
+Dim pinning_dur As Double = 14000
 Dim pinning_start_time As Double = ramp_end_time
 Dim pinning_end_time As Double = pinning_start_time + pinning_dur
 
@@ -72,7 +72,7 @@ analogdata.DisableClkDist(0.95, 4.05)
 ' quic
 analogdata.AddSmoothRamp(twodphysics_start_time, quic_init, quic_turnon_start_time, quic_turnon_end_time, ps5_ao) 'ps5_ao
 ' quad
-analogdata.AddRamp(twodphysics_start_time, quad_init, quad_turnon_start_time, quad_turnon_end_time, ps8_ao) 'ps8_ao
+analogdata.AddRamp(twodphysics_start_time, quad_init, quad_turnon_start_time, quad_turnon_end_time, ps3_ao) 'ps8_ao
 
 
 '----------------------------------------------------- () Hold on lattices -----------------------------------------------------------------
@@ -95,22 +95,40 @@ Else
 End If
 
 '2D2
-analogdata.AddInterpolatedRampUsingFile(quicTunnelRampPath, lattice2_init_v, lattice2_final_v, ramp_start_time, ramp_end_time, ps7_ao) 'lattice2D765_power2
+analogdata.AddInterpolatedRampUsingFile(quicTunnelRampPath, lattice2_init_v, lattice2_final_v, ramp_start_time, ramp_end_time, ps3_ao) 'lattice2D765_power2
 
 'quic
 analogdata.AddInterpolatedRampUsingFile(quicTiltRampPath, quic_init, quic_final, ramp_start_time, ramp_end_time, ps5_ao) 'ps5_ao
 
 'quad
-analogdata.AddInterpolatedRampUsingFile(quadTiltRampPath, quad_init, quad_final, ramp_start_time, ramp_end_time, ps3_ao) 'ps8_ao
+analogdata.AddInterpolatedRampUsingFile(quadTiltRampPath, quad_init, quad_final, ramp_start_time, ramp_end_time, ps8_ao) 'ps8_ao
 
+Dim ramp2_start_time As Double = ramp_end_time + 100
+Dim ramp2_end_time As Double = ramp2_start_time + ramp_dur
+analogdata.AddSmoothRamp(quad_final, quad_init, ramp_end_time, ramp2_start_time, ps8_ao) 'ps8_ao
+analogdata.AddSmoothRamp(lattice1_final_v, lattice1_init_v, ramp_end_time, ramp2_start_time, ps1_ao) 'lattice2D765_power
+
+'2D1
+analogdata.AddInterpolatedRampUsingFile(quadTunnelRampPath, lattice1_init_v, lattice1_final_v, ramp2_start_time, ramp2_end_time, ps1_ao) 'lattice2D765_power
+
+'2D2
+analogdata.AddInterpolatedRampUsingFile(quicTunnelRampPath, lattice2_init_v, lattice2_final_v, ramp2_start_time, ramp2_end_time, ps3_ao) 'lattice2D765_power2
+
+'quic
+analogdata.AddInterpolatedRampUsingFile(quicTiltRampPath, quic_init, quic_final, ramp2_start_time, ramp2_end_time, ps5_ao) 'ps5_ao
+
+'quad
+analogdata.AddInterpolatedRampUsingFile(quadTiltRampPath, quad_init, quad_final, ramp2_start_time, ramp2_end_time, ps8_ao) 'ps8_ao
 
 ''----------------------------------------------------- Pinning -------------------------------------------------------------------------------
 
 'Bring everything back up for pinning
 ' 2D1 
-analogdata.AddSmoothRamp(lattice1_final_v, lattice1_max_volt, ramp_end_time, pinning_end_time, lattice2D765_power)
+analogdata.AddSmoothRamp(lattice1_final_v, lattice1_max_volt, ramp2_end_time, pinning_end_time, lattice2D765_power)
 ' 2D2 raise to final depth and hold
-analogdata.AddSmoothRamp(lattice2_final_v, lattice2_max_volt, ramp_end_time, pinning_end_time, lattice2D765_power2)
+analogdata.AddSmoothRamp(lattice2_final_v, lattice2_max_volt, ramp2_end_time, pinning_end_time, lattice2D765_power2)
 
+analogdata.AddSmoothRamp(quad_final, quad_init, ramp2_end_time, pinning_end_time-2000, ps3_ao) 'ps8_ao
+analogdata.AddSmoothRamp(quad_init, 0, pinning_end_time-2000, pinning_end_time, ps3_ao) 'ps8_ao
 
 '---------------------------------------------------------------------------------------------------------------------------------------------
